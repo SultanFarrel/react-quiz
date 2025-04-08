c0e0fea13f9ab2e4eccdbfc1bbb7c3663b12e84b
Sultan Farrel <112816203+SultanFarrel@users.noreply.github.com>
2025-04-08 13:15:54 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src={process.env.PUBLIC_URL + "/logo.png"} alt="React logo" />
+      <img src={process.env.PUBLIC_URL + "/logo512.png"} alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );

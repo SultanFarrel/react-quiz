50b05c77f5286a40e34a2cc806084145a2195ce8
Sultan Farrel <112816203+SultanFarrel@users.noreply.github.com>
2025-04-08 11:18:30 +0700

    Update fetch to fake-api repo

--- src/components/App.js
+++ src/components/App.js
@@ -103,7 +103,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://raw.githubusercontent.com/sultanfarrel/fake-api/main/questions.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
